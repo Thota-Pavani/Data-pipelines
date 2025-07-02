000767e8a53e89278860b9c1d09acaa4d900fd0e
Thota-Pavani <pavani.t@kasmodigital.com>
2025-07-02 10:33:46 +0530

    Add files via upload

--- transform.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-def transformations(df):
-    df=df.drop_duplicates()
-    df=df.sort_values(by='registration_date',ascending=True)
-    df['customers_per_status'] = df.groupby('loyalty_status')['customer_id'].transform('count')
-    return df
-    
-    
